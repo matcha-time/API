36d3ec4f5e3251b4922350d27d2a1536b6bca4da
Rémi <rpinoit@student.42.fr>
2025-10-08 22:29:34 +0200

    server and routes

--- bin/serv/src/main.rs
+++ bin/serv/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    mms_api::router::router();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = mms_api::router::router();
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
--- crates/mms-api/src/router.rs
+++ crates/mms-api/src/router.rs
@@ -1,3 +1,19 @@
-pub fn router() {
-    println!("Routes");
+use axum::{Router, http::StatusCode, response::IntoResponse, routing::get};
+
+#[derive(Clone)]
+pub struct AppState {}
+
+pub fn router() -> Router {
+    Router::new().route("/", get(health)).fallback(handler_404)
+}
+
+async fn health() -> StatusCode {
+    StatusCode::OK
+}
+
+async fn handler_404() -> impl IntoResponse {
+    (
+        StatusCode::NOT_FOUND,
+        "The requested resource was not found",
+    )
 }
